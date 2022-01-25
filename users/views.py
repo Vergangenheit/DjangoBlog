e16f27df6ff02c65c25aab9817ee40fe4ccceb3c
Vergangenheit <lorenzo.ostano@gmail.com>
2022-01-25 16:53:32 +0100

    building register view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http.request import HttpRequest
+from .forms import RegisterForm
+
 
 # Create your views here.
+def register(request: HttpRequest):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+        return redirect("/home")
+    else:
+        form = RegisterForm()
+
+    return render(request, "users/register.html", {"form": form})
